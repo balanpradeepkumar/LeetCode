18a3d0422fe67e931a3df2f44a199eaf89e79483
Pradeep Kumar Balan <pradeep@kodepi.com>
2023-06-14 12:44:34 -0500

    128 Longest Consecutive Sequence - Javascript Solution (#12)

---
+++ arrays/js/128_longest_consecutive_sequence.js
@@ -0,0 +1,67 @@
+// Source : https://leetcode.com/problems/longest-consecutive-sequence/
+// Author : Pradeep Kumar Balan
+// Date   : 2023-06-14
+
+/*****************************************************************************************************
+128. Longest Consecutive Sequence
+
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+
+Example 2:
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+******************************************************************************************************/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const longestConsecutive = (nums) => {
+  let largestCount = 0;
+  let previous;
+
+  let count = 0;
+
+  let sorted = nums.sort((a, b) => a - b);
+  sorted = [...new Set(sorted)];
+  const lastIndex = sorted.length - 1;
+
+  for (let i = 0; i < sorted.length; i++) {
+    const current = sorted[i];
+    if (previous === undefined) {
+      previous = current;
+      count += 1;
+
+      if (i === lastIndex && count > largestCount) {
+        largestCount = count;
+      }
+      continue;
+    }
+
+    if (previous + 1 === current) {
+      count += 1;
+
+      if (i === lastIndex && count > largestCount) {
+        largestCount = count;
+      }
+    } else {
+      if (count > largestCount) {
+        largestCount = count;
+      }
+      count = 1;
+    }
+
+    previous = current;
+  }
+
+  return largestCount;
+};
+
+module.exports = longestConsecutive;
